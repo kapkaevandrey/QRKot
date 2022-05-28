104e367e64fcccbea7f7ceac3225c84ea4446611
Kapkaev Andrey <kapkaew@yandex.ru>
2022-05-28 22:07:06 +0300

    add main router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+
 from app.core.config import settings
+from app.api.routers import main_router
+
 
 app = FastAPI(
     title=settings.app_title,
     description=settings.description
 )
+app.include_router(main_router)
