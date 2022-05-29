cd74d0d28a16611392ab12ecf7be09bf46eb5526
Kapkaev Andrey <kapkaew@yandex.ru>
2022-05-29 23:02:46 +0300

    Add response model and typing input data in post and patch methods.

--- app/api/endpoints/charityproject.py
+++ app/api/endpoints/charityproject.py
@@ -2,25 +2,41 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends
 
 from app.core.db import get_async_session
+from app.core.user import current_superuser
+from app.schemas.charityproject import (
+    ProjectCreate, ProjectUpdate, ProjectRead,
+)
 
 router = APIRouter()
 
 
-@router.get('/')
+@router.get(
+    '/',
+    response_model=list[ProjectRead]
+)
 def get_all_projects(
         session: AsyncSession = Depends(get_async_session)
 ) -> int:
     return 0
 
 
-@router.post('/')
+@router.post(
+    '/',
+    response_model=ProjectRead,
+    dependencies=[Depends(current_superuser)]
+)
 def create_project(
+        data: ProjectCreate,
         session: AsyncSession = Depends(get_async_session)
 ) -> int:
     return 0
 
 
-@router.delete('/{project_id}')
+@router.delete(
+    '/{project_id}',
+    response_model=ProjectRead,
+    dependencies=[Depends(current_superuser)]
+)
 def delete_project(
         project_id: int,
         session: AsyncSession = Depends(get_async_session)
@@ -28,9 +44,14 @@ def delete_project(
     return project_id
 
 
-@router.patch('/{project_id}')
+@router.patch(
+    '/{project_id}',
+    response_model=ProjectRead,
+    dependencies=[Depends(current_superuser)]
+)
 def update_project(
         project_id: int,
+        data: ProjectUpdate,
         session: AsyncSession = Depends(get_async_session)
 ) -> int:
     return project_id
--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -2,27 +2,44 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends
 
 from app.core.db import get_async_session
-
+from app.core.user import current_user, current_superuser
+from app.schemas.donation import DonationRead, DonationCreate, DonationReadFull
+from app.schemas.user import UserRead
 
 router = APIRouter()
 
 
-@router.get('/')
-def get_donation(
+@router.get(
+    '/',
+    response_model=list[DonationReadFull],
+    dependencies=[Depends(current_superuser)]
+)
+def get_all_donations(
         session: AsyncSession = Depends(get_async_session)
 ) -> int:
     return 123
 
 
-@router.post('/')
-def get_donation(
-        session: AsyncSession = Depends(get_async_session)
+@router.post(
+    '/',
+    response_model=DonationRead,
+    dependencies=[Depends(current_user)]
+)
+def create_donation(
+        data: DonationCreate,
+        session: AsyncSession = Depends(get_async_session),
+        user: UserRead = Depends(current_user),
 ) -> int:
     return 123
 
 
-@router.get('/my/')
-def get_donation(
-        session: AsyncSession = Depends(get_async_session)
+@router.get(
+    '/my/',
+    response_model=list[DonationRead],
+    dependencies=[Depends(current_user)]
+)
+def get_user_donations(
+        session: AsyncSession = Depends(get_async_session),
+        user: UserRead = Depends(current_user),
 ) -> int:
     return 123
