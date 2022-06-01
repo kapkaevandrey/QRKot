146438195d678abd2198fa4dc40994050397527c
Kapkaev Andrey <kapkaew@yandex.ru>
2022-06-01 21:32:56 +0300

    delete reamin

--- app/models/abstract_base.py
+++ app/models/abstract_base.py
@@ -29,11 +29,7 @@ class ProjectDonation(Base):
     def is_active(self) -> bool:
         return not self.fully_invested
 
-    @property
-    def remain(self) -> int:
-        return self.full_amount - self.invested_amount
-
-    def deactivate(self):
+    async def deactivate(self):
         self.invested_amount = self.full_amount
         self.fully_invested = True
         self.close_date = datetime.now()
